18fe8436d8d3a2bd313d8115317ec96287580a89
David M Pham <davidpham@utexas.edu>
2019-12-24 16:39:07 -0600

    Updates

---
+++ precache-manifest.7c13ea7ad04ffe91f69b48c436d4e44e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c0d95a6c652b6884661888eb152b31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8952e6c53824327791f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "85adf40fd1d2ae42f87d",
+    "url": "/static/js/2.5efaa661.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5efaa661.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8952e6c53824327791f",
+    "url": "/static/js/main.326dd64e.chunk.js"
+  },
+  {
+    "revision": "9f69edae3ada352100b9",
+    "url": "/static/js/runtime-main.c4858aa2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
